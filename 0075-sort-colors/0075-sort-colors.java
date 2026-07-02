b0a1973928bb07ba4925fe3b0b8c3bf1d408c50d
Ankit Tambe <115581498+Ankit9767@users.noreply.github.com>
2026-07-02 17:04:28 +0530

    Time: 0 ms (100%), Space: 43.4 MB (59.99%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.java
+++ 0075-sort-colors/0075-sort-colors.java
@@ -23,28 +23,3 @@ class Solution {
       nums[r] = temp ;
     }
 }
-
-// class Solution {
-//     public void sortColors(int[] nums) {
-//         int l = 0 ; int mid = 0 ; int r = nums.length -1 ;
-
-//         while(mid <= r){
-//             if(nums[mid] == 0 ){
-//                 swap(nums, l++, mid++);
-//             }
-//             else if(nums[mid] == 1){
-//                 mid++ ;
-//             }
-//             else{
-//                 swap(nums, mid , r--);
-//             }
-//         }
-//     }
-
-//         public void swap(int[] nums, int i, int j) {
-
-//         int temp = nums[i];
-//         nums[i] = nums[j];
-//         nums[j] = temp;
-//     }
-// }
